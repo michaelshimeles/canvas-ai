7ddda7d1cd23cadaa8140b1893819ea8131b5619
Michael Shimeles <michaelwasihun96@gmail.com>
2025-11-20 16:37:36 -0500

    canvas page

--- app/canvas/page.tsx
+++ app/canvas/page.tsx
@@ -1,3 +1,11 @@
+"use client"
+import { useParams, useRouter } from "next/navigation"
+import { useEffect } from "react"
 export default function CanvasPage() {
+  const router = useRouter()
+  const { id } = useParams()
+  useEffect(() => {
+    router.push(`/canvas/${id}`)
+  }, [id])
   return <div>Canvas</div>
 }
